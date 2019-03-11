65c840eed464c0d2e8e9949f6bbfc87416a6c7ee
Damien Vitrac <dvitrac@pivotal.io>
2019-03-11 15:30:14 +0100

    Add start-client Web UI application
    This commit adds the sources for building the JavaScript client
    application for start.spring.io.
    
    The module is built using webpack, and then compiled resources are
    packaged within the result JAR under `/static`. Adding that dependency
    to the start-site module will make those resources available as static
    resources.
    
    The start-site module now has a "dev" profile that will look for static
    resources directly under the `"start-client/build"` folder at
    development time and will reload the page using devtools as needed.

---
+++ start-client/src/scripts/index.js
@@ -0,0 +1,103 @@
+import '../styles/index.scss';
+
+$.fn.radio = function () {
+    $(this).each(function () {
+        const options = $(this).find('.radio');
+        const optionsLinks = options.find('a');
+        const input = $(this).find('input');
+        optionsLinks.click(function () {
+            const radio = $(this).parent();
+            options.removeClass('active');
+            radio.addClass('active');
+            input.val($(this).data('value'))
+                .trigger('change');
+            return false;
+        });
+    });
+};
+
+$(function () {
+
+    const nameInput = $("input[name='name']");
+    const groupIdInput = $("input[name='groupId']");
+    const artifactIdInput = $("input[name='artifactId']");
+    const packageNameInput = $("input[name='packageName']");
+
+    const generatePackageName = function () {
+        let groupId = groupIdInput.val();
+        let artifactId = artifactIdInput.val();
+        let packageName = groupId.concat(".").concat(artifactId).replace(/-/g, '');
+        packageNameInput.val(packageName);
+    };
+
+    /**
+     * Parse hash bang parameters from a URL as key value object.
+     * For repeated parameters the last parameter is effective.
+     * If = syntax is not used the value is set to null.
+     * #!x&y=3 -> { x:null, y:3 }
+     * @param url URL to parse or null if window.location is used
+     * @return Object of key -> value mappings.
+     * @source https://gist.github.com/zaus/5201739
+     */
+    const hashbang = function (url, i, hash) {
+        url = url || window.location.href;
+        let pos = url.indexOf('#!');
+        if (pos < 0) return [];
+        let vars = [], hashes = url.slice(pos + 2).split('&');
+        for (i = hashes.length; i--;) {
+            hash = hashes[i].split('=');
+            vars.push({name: hash[0], value: hash.length > 1 ? hash[1] : null});
+        }
+        return vars;
+    };
+
+    const applyParams = function() {
+        var params = hashbang();
+        $.each(params, function( index, param ) {
+            var value = decodeURIComponent(param.value);
+            let element;
+            switch(param.name)  {
+                case 'type':
+                case 'language':
+                case 'packaging':
+                case 'javaVersion':
+                    element = $("a[data-value='" + value + "']");
+                    element.click();
+                    break;
+                case 'groupId':
+                case 'artifactId':
+                case 'name':
+                case 'description':
+                case 'packageName':
+                    element = $("input[name='" + param.name + "']");
+                    element.val(value);
+                    break;
+            }
+            element.trigger('change');
+        });
+    };
+
+    groupIdInput.on("change", function() {
+        generatePackageName();
+    });
+    artifactIdInput.on('change', function () {
+        nameInput.val($(this).val());
+        $("#baseDir").attr('value', this.value);
+        generatePackageName();
+    });
+
+    // -------------------------------
+
+    // Radio Component
+
+    $('.radios').radio();
+
+    if (navigator.appVersion.indexOf('Mac') != -1) {
+        $('#btn-generate span.shortcut').html(' &#8984; + &#9166;');
+    }
+    else {
+        $('#btn-generate span.shortcut').html(' alt + &#9166;');
+    }
+
+    applyParams();
+});
---
+++ start-client/webpack/webpack.config.prod.js
@@ -0,0 +1,50 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
+
+const commonEntries = merge({
+  entry: {
+      app: [
+          '@babel/polyfill'
+      ]
+  },
+}, common);
+
+module.exports = merge(commonEntries, {
+  mode: 'production',
+  devtool: 'source-map',
+  stats: 'errors-only',
+  bail: true,
+  output: {
+    filename: 'js/[name].js',
+    chunkFilename: 'js/[name].chunk.js'
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new Webpack.optimize.ModuleConcatenationPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.s?css/i,
+        use : [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+});
