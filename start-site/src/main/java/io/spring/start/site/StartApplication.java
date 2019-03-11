a5acc1cfd121d64a65269b53ff1c98f464a96de6
Brian Clozel <bclozel@pivotal.io>
2019-03-11 15:03:21 +0100

    Split project in start-client and start-site
    The start.spring.io application currently relies on a default Web UI
    built in. As of spring-io/initializr#854, the Initializr library does
    not ship anymore with those defaults and developers need to implement
    their own Controller and provide static assets to do so.
    
    This commit splits the current start.spring.io in two modules:
    
    * a start-site module that provides the service instance
    * a start-client module that packages the client application into a JAR
    
    For now, the Web UI is still being rendered partially on the server with
    a templating engine. In future developments, the whole Web UI might be
    rendered on the client side and the start-site module might only provide
    JSON APIs to feed the client.

--- start-site/src/main/java/io/spring/start/site/StartApplication.java
+++ start-site/src/main/java/io/spring/start/site/StartApplication.java
@@ -17,9 +17,11 @@
 package io.spring.start.site;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.spring.initializr.metadata.InitializrMetadataProvider;
 import io.spring.initializr.web.support.InitializrMetadataUpdateStrategy;
 import io.spring.start.site.extension.ProjectDescriptionCustomizerConfiguration;
 import io.spring.start.site.support.StartInitializrMetadataUpdateStrategy;
+import io.spring.start.site.web.HomeController;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
@@ -29,6 +31,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.resource.ResourceUrlProvider;
 
 /**
  * Initializr website application.
@@ -51,6 +54,12 @@ public class StartApplication {
 			RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
 		return new StartInitializrMetadataUpdateStrategy(restTemplateBuilder.build(),
 				objectMapper);
+	}
+
+	@Bean
+	public HomeController homeController(InitializrMetadataProvider metadataProvider,
+			ResourceUrlProvider resourceUrlProvider) {
+		return new HomeController(metadataProvider, resourceUrlProvider);
 	}
 
 }
