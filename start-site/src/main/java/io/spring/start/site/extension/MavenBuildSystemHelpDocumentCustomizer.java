def03c819053be1fa7fc139deace740d43bbd7cd
Jenn Strater <jenn@gradle.com>
2019-04-03 19:22:21 +0200

    remove mention of gradle enterprise from maven builds and make sure to use https in apache license headers

--- start-site/src/main/java/io/spring/start/site/extension/MavenBuildSystemHelpDocumentCustomizer.java
+++ start-site/src/main/java/io/spring/start/site/extension/MavenBuildSystemHelpDocumentCustomizer.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,8 +29,6 @@ class MavenBuildSystemHelpDocumentCustomizer implements HelpDocumentCustomizer {
 
 	@Override
 	public void customize(HelpDocument document) {
-		document.gettingStarted().addAdditionalLink("https://scans.gradle.com#maven",
-				"Free, shareable build insights for Gradle and Maven builds");
 		document.gettingStarted().addReferenceDocLink(
 				"https://maven.apache.org/guides/index.html",
 				"Official Apache Maven documentation");
--- start-site/src/test/java/io/spring/start/site/extension/MavenBuildSystemHelpDocumentCustomizerTests.java
+++ start-site/src/test/java/io/spring/start/site/extension/MavenBuildSystemHelpDocumentCustomizerTests.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -38,8 +38,7 @@ class MavenBuildSystemHelpDocumentCustomizerTests extends AbstractExtensionTests
 		ProjectStructure structure = generateProject(request);
 		List<String> lines = structure.readAllLines("HELP.md");
 		assertThat(lines).contains(
-				"* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)",
-				"* [Free, shareable build insights for Gradle and Maven builds](https://scans.gradle.com#maven)");
+				"* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)");
 		assertThat(lines).doesNotContain(
 				"* [Official Gradle documentation](https://docs.gradle.org)",
 				"* [Free, shareable build insights for Gradle and Maven builds](https://scans.gradle.com#gradle)");
