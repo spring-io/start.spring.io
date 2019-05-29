9109fa48ed680e9950a402e5b473cdc1f3aa2098
Damien Vitrac <dvitrac@pivotal.io>
2019-05-29 21:07:30 +0200

    Design 404

--- start-client/src/pages/404.js
+++ start-client/src/pages/404.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
-import { Layout } from '../components/common/layout'
+import { Header } from '../components/common/layout'
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <>
+    <Header />
+    <div className='error'>
+      <h1>
+        <span className='title'>/404</span>
+        NOT FOUND
+      </h1>
+      <p>You can navigate to the following pages:</p>
+      <ul>
+        <li>
+          <Link to='/'>Start with Spring Initializr</Link>
+        </li>
+        <li>
+          <a href='https://spring.io'>Navigate to spring.io</a>
+        </li>
+      </ul>
+    </div>
+  </>
 )
 
 export default NotFoundPage
