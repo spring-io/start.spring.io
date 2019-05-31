3d1b428c161cada74566d00db9e5d52039dd1771
Stephane Nicoll <snicoll@pivotal.io>
2019-05-31 11:17:36 +0200

    Improve configuration of Devtools
    This commit improves the build configuration when devtools is selected:
    
    * With Maven, the dependency is flagged optional
    * With Gradle, a dedicated configuration is created
    
    Closes gh-140
    Closes gh-64

---
+++ start-site/src/main/java/io/spring/start/site/extension/springboot/DevToolsMavenBuildCustomizer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.start.site.extension.springboot;
+
+import io.spring.initializr.generator.buildsystem.Dependency;
+import io.spring.initializr.generator.buildsystem.maven.MavenBuild;
+import io.spring.initializr.generator.buildsystem.maven.MavenDependency;
+import io.spring.initializr.generator.spring.build.BuildCustomizer;
+
+/**
+ * Maven {@link BuildCustomizer} that sets the "optional" flag when devtools is selected.
+ *
+ * @author Stephane Nicoll
+ */
+class DevToolsMavenBuildCustomizer implements BuildCustomizer<MavenBuild> {
+
+	@Override
+	public void customize(MavenBuild build) {
+		Dependency devtools = build.dependencies().get("devtools");
+		build.dependencies().add("devtools",
+				MavenDependency.from(devtools).optional(true));
+	}
+
+}
---
+++ start-site/src/test/java/io/spring/start/site/extension/springboot/DevToolsGradleBuildCustomizerTests.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.start.site.extension.springboot;
+
+import io.spring.initializr.web.project.ProjectRequest;
+import io.spring.start.site.extension.AbstractExtensionTests;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link DevToolsGradleBuildCustomizer}.
+ *
+ * @author Stephane Nicoll
+ */
+class DevToolsGradleBuildCustomizerTests extends AbstractExtensionTests {
+
+	@Test
+	void gradleWithDevtoolsCreateRuntimeOnlyConfiguration() {
+		ProjectRequest request = createProjectRequest("devtools");
+		generateGradleBuild(request).contains("configurations {", "\tdevelopmentOnly",
+				"\truntimeClasspath {", "\t\textendsFrom developmentOnly", "\t}", "}",
+				"\tdevelopmentOnly 'org.springframework.boot:spring-boot-devtools'");
+	}
+
+	@Test
+	void gradleWithoutDevtoolsDoesNotChangeOptional() {
+		ProjectRequest request = createProjectRequest("web");
+		generateGradleBuild(request).doesNotContain("developmentOnly");
+	}
+
+}
