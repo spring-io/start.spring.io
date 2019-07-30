e11632ef69e6d746f49174d27f4fa5a1b4330d60
Damien Vitrac <dvitrac@pivotal.io>
2019-07-30 11:53:33 +0200

    Update Footer structure
    Related to #186

--- start-client/src/components/common/layout/Footer.js
+++ start-client/src/components/common/layout/Footer.js
@@ -7,23 +7,29 @@ const Footer = () => (
       <br />
       start.spring.io is powered by
       <br />
-      <a
-        tabIndex='-1'
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://github.com/spring-io/initializr/'
-      >
-        Spring Initializr
-      </a>
-      {` `}and{` `}
-      <a
-        tabIndex='-1'
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://run.pivotal.io/'
-      >
-        Pivotal Web Services
-      </a>
+      <span>
+        <a
+          tabIndex='-1'
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://github.com/spring-io/initializr/'
+        >
+          Spring Initializr
+        </a>
+      </span>
+      <span>
+        {` `}and{` `}
+      </span>
+      <span>
+        <a
+          tabIndex='-1'
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://run.pivotal.io/'
+        >
+          Pivotal Web Services
+        </a>
+      </span>
     </div>
   </footer>
 )
