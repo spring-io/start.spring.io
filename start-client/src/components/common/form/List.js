f4aee3e8fea45365ef1e128f9ed8db187d401518
Damien Vitrac <dvitrac@pivotal.io>
2019-09-06 16:11:32 +0200

    Add animations
    Animation on dep. typehead, dep. selected and help menu

--- start-client/src/components/common/form/List.js
+++ start-client/src/components/common/form/List.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import { IconCheck, IconTimes } from './../icons'
 import { isInRange, rangeToText } from './../../utils/versions'
@@ -14,46 +15,67 @@ class List extends React.Component {
   render() {
     let dependencies = this.props.list
     return (
-      <div className='dependencies-list dependencies-list-checked'>
-        {dependencies.map(dependency => {
+      <TransitionGroup
+        component='ul'
+        className='dependencies-list dependencies-list-checked'
+      >
+        {dependencies.map((dependency, index) => {
           const compatibility = dependency.versionRange
             ? isInRange(this.props.boot, dependency.versionRange)
             : true
           return (
-            <a
-              className={`dependency-item checked ${
-                !compatibility ? 'invalid' : ''
-              }`}
-              href='/'
-              onClick={event => {
-                this.onClick(dependency, event)
-              }}
-              key={dependency.id}
+            <CSSTransition
+              timeout={500}
+              classNames='fade'
+              key={`f${dependency.id}`}
             >
-              <div key={`d1${dependency.id}`}>
-                <strong key={`d2${dependency.id}`}>{dependency.name}</strong>
-                <br key={`d3${dependency.id}`} />
+              <li>
+                <span className='dependency-item-span'>
+                  <a
+                    className={`dependency-item checked ${
+                      !compatibility ? 'invalid' : ''
+                    }`}
+                    href='/'
+                    onClick={event => {
+                      this.onClick(dependency, event)
+                    }}
+                    key={dependency.id}
+                  >
+                    <div key={`d1${dependency.id}`}>
+                      <strong key={`d2${dependency.id}`}>
+                        {dependency.name}
+                      </strong>
+                      <br key={`d3${dependency.id}`} />
 
-                {compatibility && (
-                  <span key={`d4${dependency.id}`} className='description'>
-                    {dependency.description}
-                  </span>
-                )}
-                <span key={`d5${dependency.id}`} className='icon'>
-                  <IconTimes key={`d6${dependency.id}`} />
-                  <IconCheck key={`d7${dependency.id}`} />
+                      {compatibility && (
+                        <span
+                          key={`d4${dependency.id}`}
+                          className='description'
+                        >
+                          {dependency.description}
+                        </span>
+                      )}
+                      <span key={`d5${dependency.id}`} className='icon'>
+                        <IconTimes key={`d6${dependency.id}`} />
+                        <IconCheck key={`d7${dependency.id}`} />
+                      </span>
+                      {!compatibility && (
+                        <span
+                          className='warning'
+                          key={`warning${dependency.id}`}
+                        >
+                          Requires Spring Boot{' '}
+                          {rangeToText(dependency.versionRequirement)}.
+                        </span>
+                      )}
+                    </div>
+                  </a>
                 </span>
-                {!compatibility && (
-                  <span className='warning' key={`warning${dependency.id}`}>
-                    Requires Spring Boot{' '}
-                    {rangeToText(dependency.versionRequirement)}.
-                  </span>
-                )}
-              </div>
-            </a>
+              </li>
+            </CSSTransition>
           )
         })}
-      </div>
+      </TransitionGroup>
     )
   }
 }
--- start-client/src/components/common/typehead/SearchResultsList.js
+++ start-client/src/components/common/typehead/SearchResultsList.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 import { IconPlus } from '../icons'
 import { isInRange, rangeToText } from '../../utils/versions'
@@ -20,59 +21,76 @@ class SearchResultsList extends React.Component {
   render() {
     let dependencies = this.props.dependencies
     return (
-      <div className='dependencies-list'>
+      <TransitionGroup component='ul' className='dependencies-list'>
         {dependencies.map((dependency, index) => {
           const valid = dependency.versionRange
             ? isInRange(this.props.boot, dependency.versionRange)
             : true
+
           return (
-            <a
-              href='/'
-              className={`dependency-item dependency-item-gray ${
-                !valid ? 'invalid' : ''
-              }  ${this.props.selected === index ? 'selected' : ''}`}
-              key={`item${dependency.id}`}
-              selected={this.props.selected === index}
-              disabled={!valid}
-              onClick={e => {
-                e.preventDefault()
-                if (valid) {
-                  this.onClick(dependency)
-                }
-              }}
-              onMouseEnter={() => {
-                this.onMouseEnter(index)
-              }}
-              onMouseLeave={() => {
-                this.onMouseLeave(index)
-              }}
+            <CSSTransition
+              timeout={500}
+              classNames='fade'
+              key={`f${dependency.id}`}
             >
-              <div>
-                <strong className='title' key={`item${dependency.id}`}>
-                  {dependency.name}
-                </strong>
-                <br />
-                {valid && (
-                  <span key={`d1${dependency.id}`}>
-                    <span className='description' key={`d2${dependency.id}`}>
-                      {dependency.description}
-                    </span>
-                    <span key={`d3${dependency.id}`} className='icon'>
-                      <IconPlus key={`d4${dependency.id}`} />
-                    </span>
-                  </span>
-                )}
-                {!valid && (
-                  <span className='warning' key={`warning${dependency.id}`}>
-                    Requires Spring Boot{' '}
-                    {rangeToText(dependency.versionRequirement)}.
-                  </span>
-                )}
-              </div>
-            </a>
+              <li>
+                <span className='dependency-item-span'>
+                  <a
+                    href='/'
+                    className={`dependency-item dependency-item-gray ${
+                      !valid ? 'invalid' : ''
+                    }  ${this.props.selected === index ? 'selected' : ''}`}
+                    key={`item${dependency.id}`}
+                    selected={this.props.selected === index}
+                    disabled={!valid}
+                    onClick={e => {
+                      e.preventDefault()
+                      if (valid) {
+                        this.onClick(dependency)
+                      }
+                    }}
+                    onMouseEnter={() => {
+                      this.onMouseEnter(index)
+                    }}
+                    onMouseLeave={() => {
+                      this.onMouseLeave(index)
+                    }}
+                  >
+                    <div>
+                      <strong className='title' key={`item${dependency.id}`}>
+                        {dependency.name}
+                      </strong>
+                      <br />
+                      {valid && (
+                        <span key={`d1${dependency.id}`}>
+                          <span
+                            className='description'
+                            key={`d2${dependency.id}`}
+                          >
+                            {dependency.description}
+                          </span>
+                          <span key={`d3${dependency.id}`} className='icon'>
+                            <IconPlus key={`d4${dependency.id}`} />
+                          </span>
+                        </span>
+                      )}
+                      {!valid && (
+                        <span
+                          className='warning'
+                          key={`warning${dependency.id}`}
+                        >
+                          Requires Spring Boot{' '}
+                          {rangeToText(dependency.versionRequirement)}.
+                        </span>
+                      )}
+                    </div>
+                  </a>
+                </span>
+              </li>
+            </CSSTransition>
           )
         })}
-      </div>
+      </TransitionGroup>
     )
   }
 }
