b25019927731ee963fc75aab9c317e5f4a3f30b6
Stephane Nicoll <snicoll@pivotal.io>
2020-03-17 03:51:47 +0100

    Fix maven reference doc link with Spring Boot 2.3+
    Closes gh-405

--- start-site/src/main/java/io/spring/start/site/extension/build/maven/MavenBuildSystemHelpDocumentCustomizer.java
+++ start-site/src/main/java/io/spring/start/site/extension/build/maven/MavenBuildSystemHelpDocumentCustomizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,8 @@ import io.spring.initializr.generator.project.ProjectDescription;
 import io.spring.initializr.generator.spring.documentation.HelpDocument;
 import io.spring.initializr.generator.spring.documentation.HelpDocumentCustomizer;
 import io.spring.initializr.generator.version.Version;
+import io.spring.initializr.generator.version.VersionParser;
+import io.spring.initializr.generator.version.VersionRange;
 
 /**
  * A {@link HelpDocumentCustomizer} that adds reference links for Apache Maven.
@@ -29,19 +31,31 @@ import io.spring.initializr.generator.version.Version;
  */
 class MavenBuildSystemHelpDocumentCustomizer implements HelpDocumentCustomizer {
 
+	private static final VersionRange SPRING_BOOT_2_3_OR_LATER = VersionParser.DEFAULT.parseRange("2.3.0.M1");
+
 	private final Version springBootVersion;
+
+	private final boolean springBoot23;
 
 	MavenBuildSystemHelpDocumentCustomizer(ProjectDescription description) {
 		this.springBootVersion = description.getPlatformVersion();
+		this.springBoot23 = SPRING_BOOT_2_3_OR_LATER.match(this.springBootVersion);
 	}
 
 	@Override
 	public void customize(HelpDocument document) {
 		document.gettingStarted().addReferenceDocLink("https://maven.apache.org/guides/index.html",
 				"Official Apache Maven documentation");
-		document.gettingStarted().addReferenceDocLink(
-				String.format("https://docs.spring.io/spring-boot/docs/%s/maven-plugin/", this.springBootVersion),
+		document.gettingStarted().addReferenceDocLink(generateReferenceGuideUrl(),
 				"Spring Boot Maven Plugin Reference Guide");
+	}
+
+	private String generateReferenceGuideUrl() {
+		String baseUrlFormat = "https://docs.spring.io/spring-boot/docs/%s/maven-plugin/";
+		if (this.springBoot23) {
+			baseUrlFormat = baseUrlFormat + "html/";
+		}
+		return String.format(baseUrlFormat, this.springBootVersion);
 	}
 
 }
