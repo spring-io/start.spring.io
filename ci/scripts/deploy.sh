551dd7e7cfea75a3e9370fe64312d81be58ef6e9
Madhura Bhave <mbhave@pivotal.io>
2020-06-23 11:48:55 -0700

    Fix elastic uri secret

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -6,5 +6,5 @@ echo $KEY > key.json
 export GOOGLE_APPLICATION_CREDENTIALS=key.json
 export KUBECONFIG=${CONFIG_DIR}/kubeconfig.yml
 
-ytt -f ${CONFIG_DIR}/secret.yml -f ${CONFIG_DIR}/elastic.yml --data-values-env TEST | kubectl apply -f-
+ytt -f ${CONFIG_DIR}/secret.yml -f ${CONFIG_DIR}/elastic.yml --data-values-env ELASTIC | kubectl apply -f-
 kubectl apply -f ${CONFIG_DIR}/deployment.yml
