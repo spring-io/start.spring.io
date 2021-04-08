24b6788bd4e3b0486f7ec71080e295e1855806a2
Stephane Nicoll <snicoll@vmware.com>
2021-04-08 17:46:35 +0200

    Polish "Tolerate null Spring Boot versions"
    See gh-667

--- start-site/src/main/java/io/spring/start/site/support/StartInitializrMetadataUpdateStrategy.java
+++ start-site/src/main/java/io/spring/start/site/support/StartInitializrMetadataUpdateStrategy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,7 @@
 
 package io.spring.start.site.support;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -44,8 +42,8 @@ public class StartInitializrMetadataUpdateStrategy extends SaganInitializrMetada
 	@Override
 	protected List<DefaultMetadataElement> fetchSpringBootVersions(String url) {
 		List<DefaultMetadataElement> versions = super.fetchSpringBootVersions(url);
-		return Optional.ofNullable(versions).orElse(Collections.emptyList()).stream().filter(this::isCompatibleVersion)
-				.collect(Collectors.toList());
+		return (versions != null) ? versions.stream().filter(this::isCompatibleVersion).collect(Collectors.toList())
+				: null;
 	}
 
 	private boolean isCompatibleVersion(DefaultMetadataElement versionMetadata) {
---
+++ start-site/src/test/java/io/spring/start/site/support/StartInitializrMetadataUpdateStrategyTests.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2012-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.start.site.support;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.spring.initializr.generator.test.InitializrMetadataTestBuilder;
+import io.spring.initializr.metadata.DefaultMetadataElement;
+import io.spring.initializr.metadata.InitializrMetadata;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+
+/**
+ * Tests for {@link StartInitializrMetadataUpdateStrategy}.
+ *
+ * @author Stephane Nicoll
+ */
+class StartInitializrMetadataUpdateStrategyTests {
+
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
+	private RestTemplate restTemplate;
+
+	private MockRestServiceServer mockServer;
+
+	@BeforeEach
+	void setUp() {
+		this.restTemplate = new RestTemplate();
+		this.mockServer = MockRestServiceServer.createServer(this.restTemplate);
+	}
+
+	@Test
+	void eolVersionsAreRemoved() {
+		InitializrMetadata metadata = new InitializrMetadataTestBuilder().addBootVersion("0.0.9.RELEASE", true)
+				.addBootVersion("0.0.8.RELEASE", false).build();
+		assertThat(metadata.getBootVersions().getDefault().getId()).isEqualTo("0.0.9.RELEASE");
+		StartInitializrMetadataUpdateStrategy provider = new StartInitializrMetadataUpdateStrategy(this.restTemplate,
+				objectMapper);
+		expectJson(metadata.getConfiguration().getEnv().getSpringBootMetadataUrl(),
+				"metadata/sagan/spring-boot-old.json");
+		InitializrMetadata updatedMetadata = provider.update(metadata);
+		assertThat(updatedMetadata.getBootVersions()).isNotNull();
+		List<DefaultMetadataElement> updatedBootVersions = updatedMetadata.getBootVersions().getContent();
+		assertThat(updatedBootVersions).hasSize(3);
+		assertBootVersion(updatedBootVersions.get(0), "2.5.0 (M1)", false);
+		assertBootVersion(updatedBootVersions.get(1), "2.4.5 (SNAPSHOT)", false);
+		assertBootVersion(updatedBootVersions.get(2), "2.4.4", true);
+	}
+
+	@Test
+	void noVersionsAreHandled() {
+		StartInitializrMetadataUpdateStrategy provider = new StartInitializrMetadataUpdateStrategy(this.restTemplate,
+				objectMapper);
+		List<DefaultMetadataElement> elements = provider.fetchSpringBootVersions(null);
+		assertThat(elements).isNull();
+	}
+
+	private static void assertBootVersion(DefaultMetadataElement actual, String name, boolean defaultVersion) {
+		assertThat(actual.getName()).isEqualTo(name);
+		assertThat(actual.isDefault()).isEqualTo(defaultVersion);
+	}
+
+	private void expectJson(String url, String bodyPath) {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		this.mockServer.expect(requestTo(url)).andExpect(method(HttpMethod.GET))
+				.andRespond(withStatus(HttpStatus.OK).body(new ClassPathResource(bodyPath)).headers(httpHeaders));
+	}
+
+}
