f5ee3adb71b1433aa5a81b17d2feacbf5666b87e
Stephane Nicoll <snicoll@vmware.com>
2021-08-25 18:10:11 +0200

    Rename Spring Buildmaster account to Spring Builds

--- ci/scripts/update-deployment.sh
+++ ci/scripts/update-deployment.sh
@@ -9,8 +9,8 @@ pushd updated-git-repo > /dev/null
 CONFIG_DIR=ci/config
 sed -i -e "s|image: .*|image: ${IMAGE}|" ${CONFIG_DIR}/deployment.yml
 
-git config user.name "Spring Buildmaster" > /dev/null
-git config user.email "spring-buildmaster@users.noreply.github.com" > /dev/null
+git config user.name "Spring Builds" > /dev/null
+git config user.email "spring-builds@users.noreply.github.com" > /dev/null
 git add ${CONFIG_DIR}/deployment.yml > /dev/null
 git commit -m"Update image digest in deployment.yml" > /dev/null
 
