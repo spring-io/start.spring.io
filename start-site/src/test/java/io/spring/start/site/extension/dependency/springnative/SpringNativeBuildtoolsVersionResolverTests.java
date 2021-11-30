790ee8203da81a1863e56ab70487276c295b0399
Stephane Nicoll <snicoll@vmware.com>
2021-11-30 15:49:17 +0100

    Upgrade to Native Buildtools 0.9.8
    Closes gh-805

--- start-site/src/test/java/io/spring/start/site/extension/dependency/springnative/SpringNativeBuildtoolsVersionResolverTests.java
+++ start-site/src/test/java/io/spring/start/site/extension/dependency/springnative/SpringNativeBuildtoolsVersionResolverTests.java
@@ -40,8 +40,8 @@ class SpringNativeBuildtoolsVersionResolverTests {
 
 	private static Stream<Arguments> springNativeVersions() {
 		return Stream.of(versions("0.9.2", null), versions("0.10.4", "0.9.3"), versions("0.10.5", "0.9.4"),
-				versions("0.11.0-M1", "0.9.4"), versions("0.11.0-M2", "0.9.7.1"),
-				versions("0.11.0-SNAPSHOT", "0.9.7.1"));
+				versions("0.11.0-M1", "0.9.4"), versions("0.11.0-M2", "0.9.7.1"), versions("0.11.0-RC1", "0.9.8"),
+				versions("0.11.0-SNAPSHOT", "0.9.8"));
 	}
 
 	private static Arguments versions(String springNativeVersion, String nativeBuildToolsVersion) {
