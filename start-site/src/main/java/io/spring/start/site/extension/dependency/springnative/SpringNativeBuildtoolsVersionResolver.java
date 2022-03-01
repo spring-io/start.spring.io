33a755637ecae9406f6a25b7cdcffaef38deb87c
Stephane Nicoll <snicoll@vmware.com>
2022-03-01 14:35:54 +0100

    Upgrade to Spring Native 0.11.3
    Closes gh-860

--- start-site/src/main/java/io/spring/start/site/extension/dependency/springnative/SpringNativeBuildtoolsVersionResolver.java
+++ start-site/src/main/java/io/spring/start/site/extension/dependency/springnative/SpringNativeBuildtoolsVersionResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,8 @@ abstract class SpringNativeBuildtoolsVersionResolver {
 			new NativeBuildtoolsRange("[0.10.0-M1, 0.10.5)", "0.9.3"),
 			new NativeBuildtoolsRange("[0.10.5, 0.11.0-M2)", "0.9.4"),
 			new NativeBuildtoolsRange("[0.11.0-M2, 0.11.0-RC1)", "0.9.7.1"),
-			new NativeBuildtoolsRange("[0.11.0-RC1, 0.11.1)", "0.9.8"), new NativeBuildtoolsRange("0.11.1", "0.9.9"));
+			new NativeBuildtoolsRange("[0.11.0-RC1, 0.11.1)", "0.9.8"),
+			new NativeBuildtoolsRange("[0.11.1,0.11.3)", "0.9.9"), new NativeBuildtoolsRange("0.11.3", "0.9.10"));
 
 	static String resolve(String springNativeVersion) {
 		Version nativeVersion = VersionParser.DEFAULT.parse(springNativeVersion);
