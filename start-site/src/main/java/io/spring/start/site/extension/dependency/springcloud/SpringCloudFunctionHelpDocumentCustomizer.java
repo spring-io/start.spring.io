176b1b39582e5dfdeaa383afe3a6d0ae4b368f93
Dennis Kieselhorst <mail@dekies.de>
2022-03-10 14:27:33 +0100

    Adding Amazon Web Services (AWS) back (#783), partially reverts commit cd6d17b1

--- start-site/src/main/java/io/spring/start/site/extension/dependency/springcloud/SpringCloudFunctionHelpDocumentCustomizer.java
+++ start-site/src/main/java/io/spring/start/site/extension/dependency/springcloud/SpringCloudFunctionHelpDocumentCustomizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2021 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -129,7 +129,8 @@ class SpringCloudFunctionHelpDocumentCustomizer implements HelpDocumentCustomize
 	 */
 	enum CloudPlatform {
 
-		AZURE("Microsoft Azure", "azure-support", Collections.singletonList(MavenBuildSystem.ID));
+		AWS("AWS Lambda", "cloud-aws"), AZURE("Microsoft Azure", "azure-support",
+				Collections.singletonList(MavenBuildSystem.ID));
 
 		private final String name;
 
