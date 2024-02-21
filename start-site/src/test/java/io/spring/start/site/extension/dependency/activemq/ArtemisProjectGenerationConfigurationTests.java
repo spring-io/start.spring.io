4d083739e931f185330ca614d171f16f0d300f2a
Eddú Meléndez <eddu.melendez@gmail.com>
2024-02-21 11:58:14 -0600

    Fix checkstyle

--- start-site/src/test/java/io/spring/start/site/extension/dependency/activemq/ArtemisProjectGenerationConfigurationTests.java
+++ start-site/src/test/java/io/spring/start/site/extension/dependency/activemq/ArtemisProjectGenerationConfigurationTests.java
@@ -19,8 +19,8 @@ package io.spring.start.site.extension.dependency.activemq;
 import io.spring.initializr.generator.test.project.ProjectStructure;
 import io.spring.initializr.web.project.ProjectRequest;
 import io.spring.start.site.extension.AbstractExtensionTests;
-
 import org.junit.jupiter.api.Test;
+
 import org.springframework.core.io.ClassPathResource;
 
 import static org.assertj.core.api.Assertions.assertThat;
