4cd0b2a57dbd2a8d92ae512ebfe004ec7c6f2eeb
Eddú Meléndez <eddu.melendez@gmail.com>
2024-02-28 12:09:56 -0600

    Fix typo

--- start-site/src/test/java/io/spring/start/site/extension/dependency/postgresql/PostgresqlProjectGenerationConfigurationTests.java
+++ start-site/src/test/java/io/spring/start/site/extension/dependency/postgresql/PostgresqlProjectGenerationConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Moritz Halbritter
  */
-class postgresqlProjectGenerationConfigurationTests extends AbstractExtensionTests {
+class PostgresqlProjectGenerationConfigurationTests extends AbstractExtensionTests {
 
 	@Test
 	void doesNothingWithoutDockerCompose() {
