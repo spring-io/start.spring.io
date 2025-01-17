c3b83ad0bcb8bc1cc3e892cb062a646c10b489d0
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-01-17 08:25:47 +0100

    Enable milestone repositories for Boot snapshot versions

--- start-site-verification/src/test/java/io/spring/start/site/MetadataVerificationTests.java
+++ start-site-verification/src/test/java/io/spring/start/site/MetadataVerificationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -143,6 +143,7 @@ class MetadataVerificationTests {
 			String qualifier = bootVersion.getQualifier().getId();
 			if (qualifier.contains("SNAPSHOT")) {
 				repositories.computeIfAbsent("spring-snapshots", this::repositoryForId);
+				repositories.computeIfAbsent("spring-milestones", this::repositoryForId);
 			}
 			else if (MILESTONE_PATTERN.matcher(qualifier).matches()) {
 				repositories.computeIfAbsent("spring-milestones", this::repositoryForId);
