d42aa90e7d3741ce7d5c1234dfe073e51cd1ccb3
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-02-17 08:54:05 +0100

    Add support for Spring AI's MCP client and server
    Closes gh-1726

---
+++ start-site/src/main/java/io/spring/start/site/extension/dependency/springai/SpringAiMcpProjectGenerationConfiguration.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.start.site.extension.dependency.springai;
+
+import io.spring.initializr.generator.buildsystem.Build;
+import io.spring.initializr.generator.buildsystem.Dependency;
+import io.spring.initializr.generator.condition.ConditionalOnRequestedDependency;
+import io.spring.initializr.generator.project.ProjectGenerationConfiguration;
+import io.spring.initializr.generator.spring.build.BuildCustomizer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for generation of projects that depend on MCP server or client.
+ *
+ * @author Moritz Halbritter
+ */
+@ProjectGenerationConfiguration
+class SpringAiMcpProjectGenerationConfiguration {
+
+	@Configuration(proxyBeanMethods = false)
+	@ConditionalOnRequestedDependency(SpringAiMcpServer.DEPENDENCY_ID)
+	static class SpringAiMcpServer {
+
+		static final String DEPENDENCY_ID = "spring-ai-mcp-server";
+
+		@Bean
+		@ConditionalOnRequestedDependency("web")
+		BuildCustomizer<Build> springAiMcpServerWebMvc() {
+			return (build) -> {
+				Dependency dependency = build.dependencies().get(DEPENDENCY_ID);
+				build.dependencies().remove(DEPENDENCY_ID);
+				build.dependencies()
+					.add(DEPENDENCY_ID, dependency.getGroupId(), "spring-ai-mcp-server-webmvc-spring-boot-starter",
+							dependency.getScope());
+			};
+		}
+
+		@Bean
+		@ConditionalOnRequestedDependency("webflux")
+		BuildCustomizer<Build> springAiMcpServerWebFlux() {
+			return (build) -> {
+				Dependency dependency = build.dependencies().get(DEPENDENCY_ID);
+				build.dependencies().remove(DEPENDENCY_ID);
+				build.dependencies()
+					.add(DEPENDENCY_ID, dependency.getGroupId(), "spring-ai-mcp-server-webflux-spring-boot-starter",
+							dependency.getScope());
+			};
+		}
+
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@ConditionalOnRequestedDependency(SpringAiMcpClient.DEPENDENCY_ID)
+	static class SpringAiMcpClient {
+
+		static final String DEPENDENCY_ID = "spring-ai-mcp-client";
+
+		@Bean
+		@ConditionalOnRequestedDependency("webflux")
+		BuildCustomizer<Build> springAiMcpClientWebFlux() {
+			return (build) -> {
+				Dependency dependency = build.dependencies().get(DEPENDENCY_ID);
+				build.dependencies().remove(DEPENDENCY_ID);
+				build.dependencies()
+					.add(DEPENDENCY_ID, dependency.getGroupId(), "spring-ai-mcp-client-webflux-spring-boot-starter",
+							dependency.getScope());
+			};
+		}
+
+	}
+
+}
---
+++ start-site/src/test/java/io/spring/start/site/extension/dependency/springai/SpringAiMcpProjectGenerationConfigurationTests.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.start.site.extension.dependency.springai;
+
+import io.spring.initializr.web.project.ProjectRequest;
+import io.spring.start.site.SupportedBootVersion;
+import io.spring.start.site.extension.AbstractExtensionTests;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link SpringAiMcpProjectGenerationConfiguration}.
+ *
+ * @author Moritz Halbritter
+ */
+class SpringAiMcpProjectGenerationConfigurationTests extends AbstractExtensionTests {
+
+	private static final SupportedBootVersion BOOT_VERSION = SupportedBootVersion.latest();
+
+	@Test
+	void shouldUseMvcServerIfWebMvcIsSelected() {
+		ProjectRequest project = createProject("web", "spring-ai-mcp-server");
+		assertThat(mavenPom(project)).hasDependency("org.springframework.ai",
+				"spring-ai-mcp-server-webmvc-spring-boot-starter");
+	}
+
+	@Test
+	void shouldUseWebFluxServerIfWebFluxIsSelected() {
+		ProjectRequest project = createProject("webflux", "spring-ai-mcp-server");
+		assertThat(mavenPom(project)).hasDependency("org.springframework.ai",
+				"spring-ai-mcp-server-webflux-spring-boot-starter");
+	}
+
+	@Test
+	void shouldUseStandardServerIfNeitherWebMvcNorWebFluxIsSelected() {
+		ProjectRequest project = createProject("spring-ai-mcp-server");
+		assertThat(mavenPom(project)).hasDependency("org.springframework.ai",
+				"spring-ai-mcp-server-spring-boot-starter");
+	}
+
+	@Test
+	void shouldUseWebFluxClientIfWebfluxIsSelected() {
+		ProjectRequest project = createProject("webflux", "spring-ai-mcp-client");
+		assertThat(mavenPom(project)).hasDependency("org.springframework.ai",
+				"spring-ai-mcp-client-webflux-spring-boot-starter");
+	}
+
+	@Test
+	void shouldUseStandardClientIfWebFluxIsNotSelected() {
+		ProjectRequest project = createProject("spring-ai-mcp-client");
+		assertThat(mavenPom(project)).hasDependency("org.springframework.ai",
+				"spring-ai-mcp-client-spring-boot-starter");
+	}
+
+	private ProjectRequest createProject(String... styles) {
+		ProjectRequest projectRequest = createProjectRequest(BOOT_VERSION, styles);
+		projectRequest.setLanguage("java");
+		return projectRequest;
+	}
+
+}
