7591f77892561fc87fe1585b0a0fb49bf7de64fd
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-03-11 09:24:08 +0100

    Fix typo

--- start-site/src/main/java/io/spring/start/site/extension/dependency/graphql/SpringGraphQlBuildCustomizer.java
+++ start-site/src/main/java/io/spring/start/site/extension/dependency/graphql/SpringGraphQlBuildCustomizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ import io.spring.initializr.metadata.InitializrMetadata;
 
 /**
  * A {@link BuildCustomizer} that automatically adds "spring-graphql-test" when the
- * {@code graphql} dependency. Additionnally, a {@code spring-webflux} dependency is added
+ * {@code graphql} dependency. Additionally, a {@code spring-webflux} dependency is added
  * in the test scope if not already present.
  *
  * @author Brian Clozel
