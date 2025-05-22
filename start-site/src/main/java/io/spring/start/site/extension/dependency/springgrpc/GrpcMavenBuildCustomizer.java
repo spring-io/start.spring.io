95c93fa57b1163be4cfcd3917071fb0651e9daf9
Dave Syer <david.syer@broadcom.com>
2025-05-22 13:46:46 +0000

    Switch gRPC plugin to io.github.ascopes:protobuf-maven-plugin
    See gh-1805
    
    Signed-off-by: Dave Syer <david.syer@broadcom.com>

--- start-site/src/main/java/io/spring/start/site/extension/dependency/springgrpc/GrpcMavenBuildCustomizer.java
+++ start-site/src/main/java/io/spring/start/site/extension/dependency/springgrpc/GrpcMavenBuildCustomizer.java
@@ -29,9 +29,7 @@ import io.spring.initializr.generator.version.VersionProperty;
  */
 class GrpcMavenBuildCustomizer implements BuildCustomizer<MavenBuild> {
 
-	private static final String OS_PLUGIN_VERSION = "1.7.1";
-
-	private static final String PROTOBUF_PLUGIN_VERSION = "0.6.1";
+	private static final String PROTOBUF_PLUGIN_VERSION = "3.3.1";
 
 	private final String protobufJavaVersion;
 
@@ -47,7 +45,6 @@ class GrpcMavenBuildCustomizer implements BuildCustomizer<MavenBuild> {
 		VersionProperty protobufJava = VersionProperty.of("protobuf-java.version");
 		VersionProperty grpc = VersionProperty.of("grpc.version");
 		addVersionProperties(build.properties(), protobufJava, grpc);
-		addOsPlugin(build.plugins());
 		addProtobufPlugin(build.plugins(), protobufJava, grpc);
 	}
 
@@ -57,30 +54,24 @@ class GrpcMavenBuildCustomizer implements BuildCustomizer<MavenBuild> {
 		properties.version(grpc, this.grpcVersion);
 	}
 
-	private void addOsPlugin(MavenPluginContainer plugins) {
-		plugins.add("kr.motd.maven", "os-maven-plugin", (plugin) -> {
-			plugin.version(OS_PLUGIN_VERSION);
-			plugin.execution("initialize", (execution) -> {
-				execution.phase("initialize");
-				execution.goal("detect");
-			});
-		});
-	}
-
 	private void addProtobufPlugin(MavenPluginContainer plugins, VersionProperty protobufJava, VersionProperty grpc) {
-		plugins.add("org.xolstice.maven.plugins", "protobuf-maven-plugin", (plugin) -> {
+		plugins.add("io.github.ascopes", "protobuf-maven-plugin", (plugin) -> {
 			plugin.version(PROTOBUF_PLUGIN_VERSION);
 			plugin.configuration((configuration) -> {
-				configuration.add("protocArtifact", "com.google.protobuf:protoc:${%s}:exe:${os.detected.classifier}"
-					.formatted(protobufJava.toStandardFormat()));
-				configuration.add("pluginId", "grpc-java");
-				configuration.add("pluginArtifact", "io.grpc:protoc-gen-grpc-java:${%s}:exe:${os.detected.classifier}"
-					.formatted(grpc.toStandardFormat()));
+				configuration.add("sourceDirectories", (builder) -> builder.add("sourceDirectory", "src/main/proto"));
+				configuration.add("protocVersion", "${%s}".formatted(protobufJava.toStandardFormat()));
+				configuration.add("binaryMavenPlugins", (builder) -> {
+					builder.add("binaryMavenPlugin", (binary) -> {
+						binary.add("groupId", "io.grpc");
+						binary.add("artifactId", "protoc-gen-grpc-java");
+						binary.add("version", "${%s}".formatted(grpc.toStandardFormat()));
+						binary.add("options", "jakarta_omit,@generated=omit");
+					});
+				});
 			});
-			plugin.execution("compile", (execution) -> {
-				execution.goal("compile").goal("compile-custom");
-				execution.configuration(
-						(configuration) -> configuration.add("pluginParameter", "jakarta_omit,@generated=omit"));
+			plugin.execution("generate", (execution) -> {
+				execution.m2e("execute onConfiguration,onIncremental");
+				execution.goal("generate");
 			});
 		});
 	}
