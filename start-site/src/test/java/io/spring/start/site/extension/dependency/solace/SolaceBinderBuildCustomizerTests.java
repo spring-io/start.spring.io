23419024f90441de8ab0519a5923e6615de9b72a
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-10-02 08:28:20 +0200

    Upgrade to Solace Spring Boot 2.5.0 and Solace Spring Cloud 4.10.0
    Both entries are now Spring Boot 3.5 compatible
    
    Closes gh-1926

--- start-site/src/test/java/io/spring/start/site/extension/dependency/solace/SolaceBinderBuildCustomizerTests.java
+++ start-site/src/test/java/io/spring/start/site/extension/dependency/solace/SolaceBinderBuildCustomizerTests.java
@@ -32,7 +32,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class SolaceBinderBuildCustomizerTests extends AbstractExtensionTests {
 
-	private static final SupportedBootVersion BOOT_VERSION = SupportedBootVersion.V3_4;
+	private static final SupportedBootVersion BOOT_VERSION = SupportedBootVersion.latest();
 
 	@Test
 	void binderNotAddedWhenSolaceNotSelected() {
