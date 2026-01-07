ef013cd2f38fac239d392e02100c112081425892
Moritz Halbritter <moritz.halbritter@broadcom.com>
2026-01-07 11:56:03 +0100

    Upgrade to Spring Shell 4.0.0
    Spring Shell 4.0.0 is used for Spring Boot 4.0.x projects.
    
    Closes gh-2037

--- start-site/src/test/java/io/spring/start/site/extension/dependency/springshell/SpringShellTestBuildCustomizerTests.java
+++ start-site/src/test/java/io/spring/start/site/extension/dependency/springshell/SpringShellTestBuildCustomizerTests.java
@@ -32,7 +32,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class SpringShellTestBuildCustomizerTests extends AbstractExtensionTests {
 
-	private static final SupportedBootVersion BOOT_VERSION = SupportedBootVersion.V3_5;
+	private static final SupportedBootVersion BOOT_VERSION = SupportedBootVersion.latest();
 
 	@Test
 	void shellTestIsAddedWithSpringShell() {
@@ -50,9 +50,8 @@ class SpringShellTestBuildCustomizerTests extends AbstractExtensionTests {
 	@Test
 	void shellTestIsNotAddedWithoutSpringShell() {
 		ProjectRequest request = createProjectRequest(BOOT_VERSION, "web");
-		assertThat(mavenPom(request)).hasDependency(Dependency.createSpringBootStarter("web"))
-			.hasDependency(Dependency.createSpringBootStarter("test", Dependency.SCOPE_TEST))
-			.hasDependenciesSize(2);
+		Dependency dependency = springShellStarterTest();
+		assertThat(mavenPom(request)).doesNotHaveDependency(dependency.getGroupId(), dependency.getArtifactId());
 	}
 
 	private static Dependency springShellStarterTest() {
